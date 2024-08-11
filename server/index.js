e788a3b60b5b6396a0bd5dad8667d423d00d8e73
benshabbat <benshabbat27@gmail.com>
2024-08-11 10:08:35 +0300

    fix buggs

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
+const PORT = "5000";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.listen(8800, () => console.log(`Server running on port ${PORT}`));
