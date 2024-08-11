40ce63b3d568fee1ec623f880dc995bd8b864834
benshabbat <benshabbat27@gmail.com>
2024-08-11 10:57:27 +0300

    create client vite

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register,login,logout } from "../controllers/authController";
+import { register,login,logout } from "../controllers/authController.js";
 const router = express.Router();
 
 router.post("/register",register );
