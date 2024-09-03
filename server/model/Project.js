931f26cbdf8f4915e8c9368c25bcefcd9af7e75d
benshabbat <benshabbat27@gmail.com>
2024-09-03 21:33:41 +0300

    create ProjectSchema

---
+++ server/model/Project.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const ProjectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required:true
+  },
+  description: {
+    type: String,
+    required:true
+  },
+  languages: 
+    [{ type: String }],
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  roles: 
+    [{ type: String }]
+  });
+
+const User = mongoose.model("Project", ProjectSchema);
+
+export default User;
--- server/model/User.js
+++ server/model/User.js
@@ -18,8 +18,8 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required:true,
   },
-  skills: [{ type: String }],
-  required:true
+  skills: {[{ type: String }],
+  required:true}
 });
 
 const User = mongoose.model("User", UserSchema);
