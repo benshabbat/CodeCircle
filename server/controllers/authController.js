9839b5a3bb78811185340e0a96f6914fa7ca75f3
benshabbat <benshabbat27@gmail.com>
2024-09-09 22:06:54 +0300

    hashedPassword

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,12 +1,24 @@
-import User from "../model/User";
+import User from "../model/User.js";
 import bcryptjs from "bcryptjs";
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie";
-export const register = async (req, res) => {
-  const { firstName,lastName,email,password}= req.body;
+import { createError } from "../utils/error.js";
 
-  const userExist = await User.findOne({email});
-  
-  if(userExist) return 
+export const register = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  const userExist = await User.findOne({ email });
+
+  // if(userExist) return createError /// TODO: to use with function of createError
+
+  if (userExist) {
+    return res.status(409)
+      .json({ success: false, message: "User already exists" });
+  }
+
+  //hash for password with bcrypt algorithm
+  const hashedPassword = await bcrypt.hash(password,12)
+
+
 };
 export const login = (req, res) => {
   console.log(res.data);
