04d23950feee95c9f7b1e148a4858b62fd8b64c1
benshabbat <benshabbat27@gmail.com>
2024-09-10 10:50:12 +0300

    checking connect to db

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
 // import userRoutes from "./routes/userRoute.js";
 // import authRoutes from "./routes/authRoute.js";
 
 const app = express();
 
 dotenv.config()
+connectDB();
 
 app.use(express.json());
 app.use(cors());
